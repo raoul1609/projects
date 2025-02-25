55dc7a820f35eba89c46cd8777ab6a98ffff2eb6
raoul1609 <elieraoulnet@gmail.com>
2025-02-25 18:06:22 +0100

    going further with the project blog

--- Blog/post/models.py
+++ Blog/post/models.py
@@ -1,9 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Category (models.Model):
     name = models.CharField (null=False, max_length=255, unique=True)
+
+    class Meta:
+        db_table = 'blog_categories'
 
     def __str__(self):
         return self.name
@@ -11,6 +15,9 @@ class Category (models.Model):
     
 class Tag(models.Model):
     name = models.CharField(max_length=255)
+
+    class Meta:
+        db_table = 'blog_tags'
    
     def __str__(self):
         return self.name
@@ -25,6 +32,10 @@ class Post (models.Model):
     # une realtion manytomany entre tag et posts
     tag = models.ManyToManyField(Tag, related_name='posts', blank=True)  #L'option related_name='posts' permet d'accéder à tous les posts associés à un tag via tag.posts
 
+
+    class Meta:
+        db_table = 'blog_posts'
+
     def __str__(self):
         return ({1} + '' + {2} + ' cree le' + {str(self.created_at)}).format (self.title, self.content)
 
@@ -38,8 +49,21 @@ class Comment (models.Model):
     content = models.TextField(null=False, default='', max_length=2000)
     created_at = models.DateTimeField(auto_now_add=True, help_text='temps de creation')
 
+    class Meta:
+        db_table = 'blog_comments'
 
     def __str__(self):
         return ("comment by {0} at {1}".format(self.author, self.created_at))
+
+
+
+class Profile(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=100)
+    profile_picture = models.ImageField()
+
+    class Meta:
+        db_table = 'blog_profiles'  #p ersonnaliser le nom de la table dans le bd
 
 
